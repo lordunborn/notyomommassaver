4a0a38147ecf793fd4223b852b63493e0d50b0a4
lordunborn <62021750+lordunborn@users.noreply.github.com>
2020-03-10 10:05:05 -0400

    Update herosaver-autoloader.user.js

--- herosaver-autoloader.user.js
+++ herosaver-autoloader.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name            Herosaver Autoloader
 // @author          TeaWithLucas
-// @namespace       https://github.com/TeaWithLucas/Herosaver/
+// @namespace       https://github.com/lordunborn/notyomommassaver/
 // @description     Autoloader for the Herosaver Script
 // @version	        1
 // @include         *example.com/*
 // @include         *forge.com/*
-// @installURL      https://raw.githubusercontent.com/TeaWithLucas/Herosaver/master/herosaver-autoloader.user.js
-// @downloadURL     https://raw.githubusercontent.com/TeaWithLucas/Herosaver/master/herosaver-autoloader.user.js
-// @updateURL       https://raw.githubusercontent.com/TeaWithLucas/Herosaver/master/herosaver-autoloader.user.js
+// @installURL      https://raw.githubusercontent.com/lordunborn/notyomommassaver/raw/master/herosaver-autoloader.user.js
+// @downloadURL     https://raw.githubusercontent.com/lordunborn/notyomommassaver/raw/master/herosaver-autoloader.user.js
+// @updateURL       https://raw.githubusercontent.com/lordunborn/notyomommassaver/raw/master/herosaver-autoloader.user.js
 // @run-at          document-end
 // ==/UserScript==
  
@@ -31,7 +31,7 @@
 function runinsert(){
 
   // run the Herosaver Script
-  var xhr=new XMLHttpRequest;xhr.open("get","https://raw.githubusercontent.com/TeaWithLucas/Herosaver/master/dist/saver.min.js",true);xhr.onreadystatechange=function(){if(xhr.readyState==4){var script=document.createElement("script");script.type="text/javascript";script.text=xhr.responseText;document.body.appendChild(script)}};xhr.send(null);
+  var xhr=new XMLHttpRequest;xhr.open("get","https://raw.githubusercontent.com/lordunborn/notyomommassaver/master/dist/saver.min.js",true);xhr.onreadystatechange=function(){if(xhr.readyState==4){var script=document.createElement("script");script.type="text/javascript";script.text=xhr.responseText;document.body.appendChild(script)}};xhr.send(null);
 
 };
 
